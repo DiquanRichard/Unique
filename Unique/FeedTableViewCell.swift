7395a0721e74f7394ba3998accefa5d7086304d7
StreetCode Academy <streetcodeacademy@SCA2-StreetCodes-MacBook-Air.local>
2017-05-22 19:37:50 -0700

    Hi

---
+++ Unique/FeedTableViewCell.swift
@@ -0,0 +1,26 @@
+//
+//  FeedTableViewCell.swift
+//  Unique
+//
+//  Created by StreetCode Academy on 5/17/17.
+//
+//
+
+import UIKit
+
+class FeedTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var WelcomeLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
